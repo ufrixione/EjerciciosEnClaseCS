842229cd1390c6511f0c87e2bf9f819f677d557a
Ulises <ufrixione@uamv.edu.ni>
2026-05-06 12:06:42 -0600

    feat: add arreglo1 project

---
+++ arreglo1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Leer 5 edades y mostrar la edad promedio
+int[] edades = new int[5];
+int suma = 0;
+double promedio = 0;
+
+for (int i = 0; i < edades.Length; i++)
+{
+    Console.WriteLine("Ingrese tu edad " + (i + 1) + ":");
+    edades[i] = int.Parse(Console.ReadLine());
+}
+foreach(int edad in edades)
+{
+    suma += edad;
+}
+promedio = (double)suma / edades.Length;
+Console.WriteLine("El promedio de las edades es: " + promedio);
