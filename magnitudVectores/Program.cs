cfd2e66cd8332f5bf95e0b8464c8f10a23044b56
Ulises <ufrixione@uamv.edu.ni>
2026-05-10 14:28:21 -0600

    Feat Magnitud de Vectores

---
+++ magnitudVectores/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] V = { 3, 4, 5, 6, 7 };
+double sumaCuadrados = 0;
+for (int i = 0; i < V.Length; i++)
+{
+sumaCuadrados += Math.Pow(V[i], 2);
+}
+double magnitude = Math.Sqrt(sumaCuadrados);
+Console.WriteLine("La magnitud del vector es: " + magnitude);
