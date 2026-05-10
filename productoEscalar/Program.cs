c7f1f1e1092b2c38ef19e5ee0ef5bcc37b47a40e
Ulises <ufrixione@uamv.edu.ni>
2026-05-10 14:39:11 -0600

    Feat: Producto Escalar

---
+++ productoEscalar/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] vectorA = { 1, 2, 3, 4, 5 };
+int[] vectorB = { 5, 4, 3, 2, 1 };
+        
+int productoEscalar = 0;
+for (int i = 0; i < vectorA.Length; i++)
+{
+int resultadoMultiplicacion = vectorA[i] * vectorB[i];
+ productoEscalar += resultadoMultiplicacion;
+Console.WriteLine($"Posición {i}: {vectorA[i]} * {vectorB[i]} = {resultadoMultiplicacion}");
+}
+Console.WriteLine($"\nEl producto escalar total es: {productoEscalar}");
